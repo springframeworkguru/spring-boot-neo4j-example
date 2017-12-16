8f14f53b0544d5cc7d2d258a0a21313c505f0ee1
springframeworkguru <john@springframework.guru>
2017-12-16 09:50:46 +0530

    Updated Spring Boot to 2.0.0.M7 with JPA updates

--- src/main/java/guru/springframework/SpringBootNeo4jApplication.java
+++ src/main/java/guru/springframework/SpringBootNeo4jApplication.java
@@ -2,6 +2,7 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
 public class SpringBootNeo4jApplication {
--- src/main/java/guru/springframework/repositories/ProductRepository.java
+++ src/main/java/guru/springframework/repositories/ProductRepository.java
@@ -1,11 +1,10 @@
 package guru.springframework.repositories;
 
 import guru.springframework.domain.Product;
-import org.springframework.data.neo4j.repository.GraphRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 /**
  * Created by jt on 1/10/17.
  */
-public interface ProductRepository extends GraphRepository<Product> {
+public interface ProductRepository extends Neo4jRepository<Product, Long> {
 }
